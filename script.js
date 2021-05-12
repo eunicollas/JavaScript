e4aa71217723ff96d79706463eeaeb02d53867e1
eunicollas <80913209+eunicollas@users.noreply.github.com>
2021-05-12 19:53:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const anoAtual = 2021
+const anodeNascimento = prompt ("Digite o ano que você nasceu")
+const nome = prompt("Digite seu nome")
+
+let apresentar = `Seu nome é ${nome} e você tem ${anoAtual - anodeNascimento} anos`
+alert(apresentar)
